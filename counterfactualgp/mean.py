267d0dd4b4e772321e0320944c44613c4c9c0bab
Jan Fan <guangyizhang.jan@gmail.com>
2018-07-24 14:51:48 +0300

    mean function model

---
+++ counterfactualgp/mean.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class LinearModel(object):
+    def __init__(self, degree):
+        self.degree = degree
+        self._coef = np.zeros(degree+1)
+
+    @property
+    def coef(self):
+        return self._coef
+
+    def predict(self, x):
+        return np.poly1d(x)
+
+    def fit(self, samples):
+        '''Pooled least square fit'''
+
+        t = np.concatenate([t for y, (t, rx) in samples])
+        y = np.concatenate([y for y, (t, rx) in samples])
+        self._coef = np.polyfit(t, y, self.degree)
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,22 @@
-# Sample Test passing with nose and pytest
+import pytest
+import numpy as np
+from counterfactualgp.mean import LinearModel
+
+
+@pytest.fixture
+def data():
+    import pickle
+    with open('dataset/data_set_linear.pkl', 'rb') as fin:
+        return pickle.load(fin)
+
 
 def test_pass():
-        assert True, "dummy sample test"
+    assert True, "dummy sample test"
+
+
+def test_linearmodel(data):
+    m = LinearModel(1)
+    m.fit(data['training'])
+    coef_ = np.round(m.coef, 1).tolist()
+    assert coef_[0] == 0.1
+    assert coef_[1] != 0.5 # bias changes after treatment 
